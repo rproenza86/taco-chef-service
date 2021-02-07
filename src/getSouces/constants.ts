22f70b26474ca403cb78f312fabaf791d7dfe05c
raul.rodriguezproenza <raul.rodriguezproenza@coxautoinc.com>
2021-02-07 17:17:30 -0500

    feat: Get tacos sauces

---
+++ src/getSouces/constants.ts
@@ -0,0 +1,2 @@
+export const SPOONCULAR_ENDPOINT = 'https://api.spoonacular.com';
+export const SPOONCULAR_API_KEY = '3c96dbdcf8a645fd830ad715ffc77da2'; // TODO: Move to parameter store
--- src/getSouces/index.ts
+++ src/getSouces/index.ts
@@ -1,7 +1,23 @@
-export const handler = async (event: any = {}): Promise<any> => {
-  // Log the event argument for debugging and for use in local development.
-  const response = JSON.stringify(event, undefined, 2);
-  console.log(response);
+// externals
+import axios from 'axios';
 
-  return response;
-}
+// utils
+import { buildResponse } from 'utils';
+
+// constants
+import { SPOONCULAR_ENDPOINT, SPOONCULAR_API_KEY } from './constants';
+
+export const handler = async () => {
+    try {
+        const sauceRecipePath = '/recipes/complexSearch';
+        const endpoint = `${SPOONCULAR_ENDPOINT}${sauceRecipePath}?apiKey=${SPOONCULAR_API_KEY}&query=sauces`;
+        const sauces = await axios.get(endpoint);
+
+        return buildResponse(200, sauces);
+    } catch (error) {
+        return buildResponse(500, {
+            message: 'Recipes not found. Error fetching sauces recipes.',
+            rawError: error,
+        });
+    }
+};
