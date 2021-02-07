56ac89839ef2282e3cfed87091d6cac86c34a711
raul.rodriguezproenza <raul.rodriguezproenza@coxautoinc.com>
2021-02-07 14:41:43 -0500

    chore: Code clean up

--- src/getSouces/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-exports.handler = async (event = {}) => {
-    // Log the event argument for debugging and for use in local development.
-    const response = JSON.stringify(event, undefined, 2);
-    console.log(response);
-    return response;
-};
