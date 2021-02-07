52a0fa1fc47d104c7b36afee7f6b1ed861593dd1
raul.rodriguezproenza <raul.rodriguezproenza@coxautoinc.com>
2021-02-07 11:10:10 -0500

    chore: Code clean up

--- src/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ImageType = void 0;
-var ImageType;
-(function (ImageType) {
-    ImageType["Jpg"] = "jpg";
-})(ImageType = exports.ImageType || (exports.ImageType = {}));
