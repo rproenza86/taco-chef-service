0dc16e72edfaf006ab2445de2d08f72b6476416c
raul.rodriguezproenza <raul.rodriguezproenza@coxautoinc.com>
2021-02-08 18:27:11 -0500

    fix: Create sauce card

--- src/createSouces/index.ts
+++ src/createSouces/index.ts
@@ -1,33 +1,46 @@
 // externals
-import axios from 'axios';
-import multipart from 'aws-lambda-multipart-parser';
+import axios, { AxiosRequestConfig } from 'axios';
+import * as multipart from 'parse-multipart-data';
 
 // utils
 import { buildResponse } from 'utils';
 
 const FormData = require('form-data');
 
-export const handler = async (
-    event: AWSLambda.APIGatewayEvent
-): Promise<any> => {
+export const handler = async (event: AWSLambda.APIGatewayEvent) => {
     try {
-        const payload = multipart(event, true);
+        const bodyBuffer = Buffer.from(event.body, 'base64');
+        const boundary = multipart.getBoundary(
+            event.headers['Content-Type'] || event.headers['content-type']
+        );
+
+        const parts = multipart.parse(bodyBuffer, boundary);
 
         const form = new FormData();
-        form.append('title', payload.title);
-        form.append('image', payload.image);
-        form.append('author', payload.author);
-        form.append('ingredients', payload.ingredients);
+        parts.forEach((part) => {
+            if (part.name) {
+                form.append(part.name, part.data.toString('utf8'));
+            }
+            if (part?.type?.includes('image')) {
+                form.append('image', part.data, { filename: part.filename });
+            }
+        });
 
         const sauceRecipePath = '/recipes/visualizeRecipe';
         const endpoint = `https://api.spoonacular.com${sauceRecipePath}?apiKey=3c96dbdcf8a645fd830ad715ffc77da2`;
-        const options = { body: event.body };
 
-        const sauces = await axios.post(endpoint, form, {
-            headers: { 'Content-Type': 'multipart/form-data' }
-        });
+        const config = {
+            method: 'post',
+            url: endpoint,
+            headers: {
+                ...form.getHeaders()
+            },
+            data: form
+        };
+        const cardRequestResult = await axios(config as AxiosRequestConfig);
+        console.log('Request done', cardRequestResult);
 
-        return buildResponse(sauces.status, sauces.data);
+        return buildResponse(cardRequestResult.status, cardRequestResult.data);
     } catch (error) {
         console.log('Error details: ', error);
 
